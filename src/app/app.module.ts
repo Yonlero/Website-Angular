1f8b057e2bbb7b2135959eac4712795517ac2daf
Yonlero <abraaobrg150@gmail.com>
2022-09-16 10:39:27 -0300

    Site structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { BottomComponent } from './footer/bottom/bottom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    FooterComponent,
+    BottomComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/footer/bottom/bottom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-bottom',
+  templateUrl: './bottom.component.html',
+  styleUrls: ['./bottom.component.css']
+})
+export class BottomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
